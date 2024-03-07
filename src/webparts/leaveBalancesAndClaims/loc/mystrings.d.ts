ba40b6c2447a1ea44dd324ebdc240b12f49912ce
Johaarosaur <johaar.omar@gmail.com>
2024-03-07 12:26:54 +0200

    Initial commit

---
+++ src/webparts/leaveBalancesAndClaims/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ILeaveBalancesAndClaimsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'LeaveBalancesAndClaimsWebPartStrings' {
+  const strings: ILeaveBalancesAndClaimsWebPartStrings;
+  export = strings;
+}
